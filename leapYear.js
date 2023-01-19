fa58a336b37579d31b4fb1fc51ef3bacbe192195
manerajeshri <manerajeshri@gmail.com>
2023-01-19 23:49:04 +0530

    method 2
    method 2

--- leapYear.js
+++ leapYear.js
@@ -1,15 +1,26 @@
 
 
+// leap year
+//A year is a leap year if the following conditions are satisfied:
 
-function leapYear(year) {
+// The year is a multiple of 400.
+// The year is a multiple of 4 and not a multiple of 100.
+
+function IsLeapYear(year) {
 
 return (year%4 == 0  || year%100 == 0 || year%400 == 0) ? "Leap Year" : "Not a Leap Year" ;
-
 }
+console.log(`Year 2014 is `,IsLeapYear(2014)); console.log(`Year 2020 is `,IsLeapYear(2020));
+console.log(`Year 2017 is `,IsLeapYear(2017)); console.log(`Year 2022 is `,IsLeapYear(2022));
+console.log(`Year 2000 is `,IsLeapYear(2000)); console.log(`Year 2004 is `,IsLeapYear(2004));
+console.log(`Year 1996 is `,IsLeapYear(1996)); console.log(`Year 1999 is `,IsLeapYear(1999));
 
-console.log(`Year 2014 is `,leapYear(2014));
-console.log(`Year 2020 is `,leapYear(2020));
-console.log(`Year 2017 is `,leapYear(2017));
-console.log(`Year 2022 is `,leapYear(2022));
-console.log(`Year 2000 is `,leapYear(2000));
-console.log(`Year 1996 is `,leapYear(1996));
+
+function IsLeapYear1(year) {
+
+    return (year%4 == 0  || (year%400 == 0 && year%100 !== 0 ) ) ? "Leap Year" : "Not a Leap Year" ;
+    }
+    console.log(`\nYear 2014 is `,IsLeapYear1(2014)); console.log(`Year 2020 is `,IsLeapYear1(2020));
+    console.log(`Year 2017 is `,IsLeapYear1(2017)); console.log(`Year 2022 is `,IsLeapYear1(2022));
+    console.log(`Year 2000 is `,IsLeapYear1(2000)); console.log(`Year 2004 is `,IsLeapYear1(2004));
+    console.log(`Year 1996 is `,IsLeapYear1(1996)); console.log(`Year 1999 is `,IsLeapYear1(1999));
