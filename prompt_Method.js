707ba5c9b8b4c4b725fcdfdb0ac916048d270d5e
manerajeshri <manerajeshri@gmail.com>
2023-01-19 23:22:41 +0530

    prompt_Method
    prompt_Method

---
+++ prompt_Method.js
@@ -0,0 +1,11 @@
+// var myName = window.prompt(`Enter Your name..?`);
+// console.log(`myName is:${myName}`);
+// console.log(`Hey ${myName} How are you..?`);
+
+
+
+// take input from the user
+const string = prompt('Enter a string: ');
+
+// const result = reverseString(string);
+console.log(string);
