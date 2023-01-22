5c09840ea3622de0d1e65b5d176108d13e603887
manerajeshri <manerajeshri@gmail.com>
2023-01-23 03:35:02 +0530

    fibonacciSeries
    method 1 for loop
    method 2 do while loop

--- Fibonacci_Series.js
+++ Fibonacci_Series.js
@@ -1,5 +1,6 @@
 
-
+// method 1 for loop
+console.log(`method 1 for loop`);
 
 function fibonacciSeries(number) {
  let n1 = 0,n2=1,nextTerm;
@@ -11,5 +12,29 @@ function fibonacciSeries(number) {
     n2=nextTerm;    
  }
 }
-
 fibonacciSeries(7);
+
+
+// method 2 do while loop 
+
+let number = 7
+console.log(`method 2 do while loop `);
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+console.log(n1); // print 0
+console.log(n2); // print 1
+
+nextTerm = n1 + n2;
+
+while (nextTerm <= number+1) {
+
+    // print the next term
+    console.log(nextTerm);
+
+    n1 = n2;
+    n2 = nextTerm;
+    nextTerm = n1 + n2;
+}
+
+
