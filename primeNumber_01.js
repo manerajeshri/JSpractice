79a57282753ee710ac3288dd17e6e57c0718f4c5
manerajeshri <manerajeshri@gmail.com>
2023-01-22 23:58:12 +0530

    update

--- primeNumber_01.js
+++ primeNumber_01.js
@@ -1,18 +1,27 @@
 function IsPrimeNumber(num) {
-    
+   let count = 0;
 for (let i = 2; i < num; i++) {
-    
-    
-    if ((num % i) == 0) {
-        console.log(`Given number is "${num}" and it is "Not a Prime" no.`);
-        break;
-    } else {
-        console.log(`Given number is  "${num}" and it is "Prime" no.`);
-        break
+    if (num % i == 0) {
+        count++;
     }
-    
+}
+if (count == 0) {
+    console.log(`Given no is ${num}: Prime Number `);
+} else {
+    console.log(`Given no is ${num}: Not a Prime Number `);
 }
 }
 
+IsPrimeNumber(0);
+IsPrimeNumber(1);
+IsPrimeNumber(2);
+IsPrimeNumber(3);
+IsPrimeNumber(4);
 IsPrimeNumber(5);
+IsPrimeNumber(6);
+IsPrimeNumber(7);
+IsPrimeNumber(8);
+IsPrimeNumber(9);
+IsPrimeNumber(10);
+IsPrimeNumber(11);
 IsPrimeNumber(30);
