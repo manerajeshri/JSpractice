2831d8f3a24f0679d5926da451ade5ca8815bd0a
manerajeshri <manerajeshri@gmail.com>
2023-01-29 23:46:02 +0530

    JavaScript function to capitalize the first letter of each word in a string.
    JavaScript function to capitalize the first letter of each word in a string.

---
+++ practice.js
@@ -0,0 +1,31 @@
+// 4.Write a JavaScript function to capitalize the first letter of each word in a string.
+// ex: input: hi how are you? output: Hi How Are You?
+console.log(`\n4.Write a JavaScript function to capitalize the first letter of each word in a string.`);
+let givenstring4 = "hi how are you?";
+console.log(`Given string is:${givenstring4}`);
+
+let x =givenstring4.trim().split(" ");
+
+console.log(x);
+
+x.forEach(element => {
+    console.log(element);
+    console.log(element.charAt(0).toUpperCase());
+    if (condition) {
+        
+    }
+});
+
+
+
+
+
+
+
+
+// function capitalizeWord(string) {
+  
+    
+    
+// }
+// capitalizeWord("hi how are you?");
