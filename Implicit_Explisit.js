13537a1ded310f163c6aa41d1a76d75918da5c6f
manerajeshri <manerajeshri@gmail.com>
2023-01-29 23:57:23 +0530

    Implicit_Explisit
    Implicit_Explisit

---
+++ Implicit_Explisit.js
@@ -0,0 +1,67 @@
+let result;
+
+// numeric string with + gives string type
+
+console.log(`1] numeric string with + gives string type`);
+
+result = '3' + 2 ;
+console.log(result);
+
+result = '3' + true ;
+console.log(result);
+
+result = '3' + undefined ;
+console.log(result);
+
+result = '3' + null ;
+console.log(result);
+
+console.log(`\n2] if boolean used true is 1 false is 0`);
+
+result = '4' - null ;
+console.log(result);
+
+result = 4 + true ;
+console.log(result);
+
+result = 4 + false ;
+console.log(result);
+
+// numeric string used with - , / , * result number
+
+result = '4' - '2';
+console.log(result);
+
+result = '4' - 2;
+console.log(result);
+
+result = '4' * '2';
+console.log(result);
+
+result = '4' / '2';
+console.log(result);
+ 
+// arithmetic operation of undefined with number , boolaen or null gives NaN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
