89033e518c1f15e973807e8f1bf68ca33004a8a7
manerajeshri <manerajeshri@gmail.com>
2023-02-03 12:14:38 +0530

    date time
    date time

---
+++ dateTime02_my.js
@@ -0,0 +1,8 @@
+let date= new Date()
+console.log(date);
+
+console.log(date.getFullYear());
+
+console.log(`Date :`,date.toLocaleDateString());
+console.log(`Date :`,date.toDateString());
+console.log(`Date :`,date.toLocaleTimeString());
