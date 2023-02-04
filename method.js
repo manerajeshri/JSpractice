f2812485f766a132b44048986f92de6f5f81acba
manerajeshri <manerajeshri@gmail.com>
2023-02-04 15:09:24 +0530

    method in object and calling method
    method in object and calling method

---
+++ method.js
@@ -0,0 +1,23 @@
+//METHOD==> function as value
+console.log(`METHOD==> function as value`);
+const person ={
+    // key : value,
+    name: "raje",
+    age : 32,
+    no: 7709830074,
+    greet : function () {console.log(`hi I am method. Object method shorthand`)} ,// 1] no return
+    greetReturn : function () {return `hi I am method. Object method shorthand`} // 2] with return
+
+}
+// 1] no return ==>  greet : function () {console.log(`hi I am method. Object method shorthand`)}
+console.log(`\n1] no return ==>  person.greet(); `);
+person.greet();
+
+
+console.log(`\n 2] with return greetReturn ==> console.log(person.greetReturn());`);
+console.log(person.greetReturn());
+
+console.log(`\n 3] storing return in variable and logging variable let msg = person.greetReturn(); 
+console.log(msg);`);
+let msg = person.greetReturn(); 
+console.log(msg);
