08edccf962988f543c333430e594f27d47dc4711
manerajeshri <manerajeshri@gmail.com>
2023-02-07 01:24:31 +0530

    Date()
    Date()

---
+++ buitInObjDate.js
@@ -0,0 +1,16 @@
+let date = new Date();
+
+console.log(date);
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.toDateString())
+console.log(date.toTimeString());
+
+var dt = new Date("Aug 28, 2008 23:30:00");
+console.log(dt);
+var d1 = new Date("2015-03-25");
+console.log(d1);
+var d2 = new Date("03/25/2016");
+console.log(d2);
+var d3 = new Date("25 Mar 2015");
+console.log(d3);
