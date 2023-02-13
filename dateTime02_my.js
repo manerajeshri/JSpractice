ca7b32846ee83204c360ee25a8e76da78c2bc492
manerajeshri <manerajeshri@gmail.com>
2023-02-13 21:04:31 +0530

    update
    update

--- dateTime02_my.js
+++ dateTime02_my.js
@@ -1,8 +1,12 @@
+// let date= new Date()
+// console.log(date);
+
+// console.log(date.getFullYear());
+
+// console.log(`Date :`,date.toLocaleDateString());
+// console.log(`Date :`,date.toDateString());
+// console.log(`Time :`,date.toLocaleTimeString());
+
 let date= new Date()
-console.log(date);
-
-console.log(date.getFullYear());
-
-console.log(`Date :`,date.toLocaleDateString());
 console.log(`Date :`,date.toDateString());
-console.log(`Date :`,date.toLocaleTimeString());
+console.log(`Time :`,date.toLocaleTimeString());
