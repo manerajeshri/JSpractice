973aaea332340f8cfd2cacc468988b45d872a36f
manerajeshri <manerajeshri@gmail.com>
2023-02-13 21:04:22 +0530

    arrayOfMovies
    arrayOfMovies

---
+++ arrayOfObject.js
@@ -0,0 +1,5 @@
+arrayOfMovies =[
+    { title: "3 Idiots", director: "Rajkumar", cast: "Amir", releaseDate: "2000"},
+    { title: "Tumbad", director: "Rahil", cast: "rahi", releaseDate: "2020"},
+    { title: "Kantara", director: "Rishab", cast: "Rishab", releaseDate: "2022"}
+  ]
