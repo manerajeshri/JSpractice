1fba5676a9058220a92c5d378ab756475720b193
manerajeshri <manerajeshri@gmail.com>
2023-02-13 21:04:44 +0530

    update
    update

--- scratch.js
+++ scratch.js
@@ -4,7 +4,7 @@ console.log(`\ mearging array`);
 var array1=[1,2,3,4];
 var array2 =["a","b","c","d"];
 
-var mergedArray = Object.assign([],array1,array2);
+var mergedArray = Object.assign([],array1,...array2);
 console.log(`Meerged array : `, mergedArray); // Meerged array :  (4) ['a', 'b', 'c', 'd']
 
 // expected output ===> [1,2,3,4,"a","b","c","d"]
