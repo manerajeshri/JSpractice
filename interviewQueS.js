47a9c0c62a7acf36f8c7bc43d2b8b7eb5f88f68b
manerajeshri <manerajeshri@gmail.com>
2023-02-17 12:10:59 +0530

    practice
    practice

---
+++ interviewQueS.js
@@ -0,0 +1,17 @@
+// I want numbers till 10 only. without using break and return and continue statement.
+
+// for(let i=1;  i<=20; i++){
+//      console.log(i) 
+// }
+
+//ans ==>
+// for(let i=1;  i<=20 && i<=10; i++){
+//      console.log(i) 
+// }
+
+
+// let str = 'hello how are you';
+//  Output should be -  'you are how hello'
+//ans ==>
+// let str = 'hello how are you';
+// console.log(str.split(' ').reverse().join(' '));
