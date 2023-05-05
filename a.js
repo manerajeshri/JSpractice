9c4a1dfed39897db49f957e8154ed57798b235d9
manerajeshri <manerajeshri@gmail.com>
2023-05-05 09:50:33 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,46 @@
+// // // function getAge () {
+// // //     'use strict';
+// // //     age = 21;
+// // //     console.log(age);
+// // // }
+// // // getAge();
+
+// // const shape = {
+// //     radius: 10,
+// //     diameter (){
+// //         return this.radius*2;
+// //     },
+// //     perimeter : () => 2 * this.radius,
+// // };
+// // console.log(shape.diameter());
+// // console.log(shape.perimeter());
+
+
+
+
+
+// let c = { greetings : "Hey!"};
+// let d;
+// d=c;
+// c.greetings = 'Hello';
+// console.log(d.greetings);
+
+
+
+// function printNumbers(first, second , first) {
+//     console.log(first, second , first);
+// }
+// printNumbers(1,2,3);
+
+
+Arr =[ 1,2,3,4,5,6,7,8,9]
+
+// Question 
+// 1) find even number 
+const evennumber = Arr.forEach(element => {
+    console.log(element);
+});
+console.log(`evennumber`, evennumber);
+// 2) find odd number
+// 3) sum of all number 
+// 4) seprate 5,6 only
